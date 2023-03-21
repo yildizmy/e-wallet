20356c4bfa87c0fc530d519b1d1186220ab40509
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-21 19:13:10 +0300

    feat: implement appconfig class

--- backend/src/main/java/com/github/yildizmy/common/Constants.java
+++ backend/src/main/java/com/github/yildizmy/common/Constants.java
@@ -7,4 +7,7 @@ public final class Constants {
 
     private Constants() {
     }
+
+    public static final String DATE_FORMAT = "dd.MM.yyyy";
+    public static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
 }
---
+++ backend/src/main/java/com/github/yildizmy/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.github.yildizmy.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+import static com.github.yildizmy.common.Constants.DATE_FORMAT;
+import static com.github.yildizmy.common.Constants.DATE_TIME_FORMAT;
+
+/**
+ * Configuration file used to configure settings of the application
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(DATE_TIME_FORMAT);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+        };
+    }
+}
