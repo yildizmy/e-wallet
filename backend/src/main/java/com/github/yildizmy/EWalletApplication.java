beb28a7880c0eb1b4d66c2f26cad8d7ebd8dc27c
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-21 14:09:44 +0300

    initial commit

---
+++ backend/src/main/java/com/github/yildizmy/EWalletApplication.java
@@ -0,0 +1,13 @@
+package com.github.yildizmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/yildizmy/EWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yildizmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
