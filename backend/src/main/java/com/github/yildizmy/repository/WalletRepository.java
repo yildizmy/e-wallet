0ba6527231f08a26b46c6be0303c213ef1a52527
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-22 15:13:29 +0300

    feat: implement wallet repository

---
+++ backend/src/main/java/com/github/yildizmy/repository/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.github.yildizmy.repository;
+
+import com.github.yildizmy.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+}
