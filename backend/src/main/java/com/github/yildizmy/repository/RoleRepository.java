e50047176890d404826fb23fcf1c38e7fa09bb1a
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-22 15:14:08 +0300

    feat: implement role repository

---
+++ backend/src/main/java/com/github/yildizmy/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.github.yildizmy.repository;
+
+import com.github.yildizmy.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
