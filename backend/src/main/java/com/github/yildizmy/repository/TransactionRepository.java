8cf6e72a5df72e15c0b0b9773c591772b7d2792c
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-22 15:14:24 +0300

    feat: implement transaction repository

---
+++ backend/src/main/java/com/github/yildizmy/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.github.yildizmy.repository;
+
+import com.github.yildizmy.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
