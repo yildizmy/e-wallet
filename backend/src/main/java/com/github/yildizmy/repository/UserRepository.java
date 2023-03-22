31bed07f231497ef56360ff44aa44267050f584e
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-22 15:08:42 +0300

    feat: implement userrepository

---
+++ backend/src/main/java/com/github/yildizmy/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.github.yildizmy.repository;
+
+import com.github.yildizmy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
