ad6b892b66444b64fd0708b374e208821822a11a
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 01:00:19 +0300

    feat: implement new method

--- backend/src/main/java/com/github/yildizmy/service/TypeService.java
+++ backend/src/main/java/com/github/yildizmy/service/TypeService.java
@@ -1,5 +1,6 @@
 package com.github.yildizmy.service;
 
+import com.github.yildizmy.model.Type;
 import com.github.yildizmy.repository.TypeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,4 +15,14 @@ import org.springframework.stereotype.Service;
 public class TypeService {
 
     private final TypeRepository typeRepository;
+
+    /**
+     * Fetches a single type reference (entity) by the given id
+     *
+     * @param id
+     * @return Type
+     */
+    public Type getReferenceById(long id) {
+        return typeRepository.getReferenceById(id);
+    }
 }
