b47ea098aaf3e9e4d8c199940bdc41bb93fb1cfa
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-24 20:57:55 +0300

    feat: implement wallet service

--- backend/src/main/java/com/github/yildizmy/service/WalletService.java
+++ backend/src/main/java/com/github/yildizmy/service/WalletService.java
@@ -1,9 +1,22 @@
 package com.github.yildizmy.service;
 
+import com.github.yildizmy.dto.mapper.WalletRequestMapper;
+import com.github.yildizmy.dto.mapper.WalletResponseMapper;
+import com.github.yildizmy.dto.request.WalletRequest;
+import com.github.yildizmy.dto.response.CommandResponse;
+import com.github.yildizmy.dto.response.WalletResponse;
+import com.github.yildizmy.exception.ElementAlreadyExistsException;
+import com.github.yildizmy.exception.NoSuchElementFoundException;
+import com.github.yildizmy.model.Wallet;
 import com.github.yildizmy.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.github.yildizmy.common.Constants.*;
 
 /**
  * Service used for Wallet related operations
@@ -14,5 +27,104 @@ import org.springframework.stereotype.Service;
 public class WalletService {
 
     private final WalletRepository walletRepository;
-}
+    private final UserService userService;
+    private final WalletRequestMapper walletRequestMapper;
+    private final WalletResponseMapper walletResponseMapper;
 
+    /**
+     * Fetches a single wallet by the given id
+     *
+     * @param id
+     * @return PetResponse
+     */
+    @Transactional(readOnly = true)
+    public WalletResponse findById(long id) {
+        return walletRepository.findById(id)
+                .map(walletResponseMapper::toDto)
+                .orElseThrow(() -> new NoSuchElementFoundException(NOT_FOUND_WALLET));
+    }
+
+    /**
+     * Fetches a single wallet by the given iban
+     *
+     * @param iban
+     * @return PetResponse
+     */
+    @Transactional(readOnly = true)
+    public WalletResponse findByIban(String iban) {
+        return walletRepository.findByIban(iban)
+                .map(walletResponseMapper::toDto)
+                .orElseThrow(() -> new NoSuchElementFoundException(NOT_FOUND_WALLET));
+    }
+
+    /**
+     * Fetches all wallets based on the given paging and sorting parameters
+     *
+     * @param pageable
+     * @return List of PetResponse
+     */
+    @Transactional(readOnly = true)
+    public Page<WalletResponse> findAll(Pageable pageable) {
+        final Page<WalletResponse> wallets = walletRepository.findAll(pageable)
+                .map(walletResponseMapper::toDto);
+        if (wallets.isEmpty())
+            throw new NoSuchElementFoundException(NOT_FOUND_RECORD);
+        return wallets;
+    }
+
+    /**
+     * Creates a new wallet using the given request parameters
+     *
+     * @param request
+     * @return id of the created wallet
+     */
+    public CommandResponse create(WalletRequest request) {
+        if (walletRepository.existsByUserIdAndIbanIgnoreCase(request.getUserId(), request.getIban()))
+            throw new ElementAlreadyExistsException(ALREADY_EXISTS_WALLET_IBAN);
+        if (walletRepository.existsByUserIdAndNameIgnoreCase(request.getUserId(), request.getName()))
+            throw new ElementAlreadyExistsException(ALREADY_EXISTS_WALLET_NAME);
+
+        final Wallet wallet = walletRequestMapper.toEntity(request, userService);
+        walletRepository.save(wallet);
+        log.info(CREATED_WALLET);
+        return CommandResponse.builder().id(wallet.getId()).build();
+    }
+
+    /**
+     * Updates wallet using the given request parameters
+     *
+     * @param request
+     * @return id of the updated wallet
+     */
+    public CommandResponse update(WalletRequest request) {
+        final Wallet foundWallet = walletRepository.findById(request.getId())
+                .orElseThrow(() -> new NoSuchElementFoundException(NOT_FOUND_WALLET));
+
+        // check if the iban is changed and new iban is already exists in user's wallets
+        if (!request.getIban().equalsIgnoreCase(foundWallet.getIban()) &&
+                walletRepository.existsByUserIdAndIbanIgnoreCase(request.getUserId(), request.getIban()))
+            throw new ElementAlreadyExistsException(ALREADY_EXISTS_WALLET_IBAN);
+
+        // check if the name is changed and new name is already exists in user's wallets
+        if (!request.getName().equalsIgnoreCase(foundWallet.getName()) &&
+                walletRepository.existsByUserIdAndNameIgnoreCase(request.getUserId(), request.getName()))
+            throw new ElementAlreadyExistsException(ALREADY_EXISTS_WALLET_NAME);
+
+        final Wallet wallet = walletRequestMapper.toEntity(request, userService);
+        walletRepository.save(wallet);
+        log.info(UPDATED_WALLET);
+        return CommandResponse.builder().id(wallet.getId()).build();
+    }
+
+    /**
+     * Deletes wallet by the given id
+     *
+     * @param id
+     */
+    public void deleteById(long id) {
+        final Wallet wallet = walletRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementFoundException(NOT_FOUND_WALLET));
+        walletRepository.delete(wallet);
+        log.info(DELETED_WALLET);
+    }
+}
