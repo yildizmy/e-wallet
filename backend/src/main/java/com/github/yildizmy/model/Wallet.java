55b801f3d6efc5ae4761b9178a875520534c3893
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-24 14:17:08 +0300

    refactor: add unique indexes

--- backend/src/main/java/com/github/yildizmy/model/Wallet.java
+++ backend/src/main/java/com/github/yildizmy/model/Wallet.java
@@ -14,6 +14,10 @@ import java.util.Set;
 @Setter
 @Entity
 @EqualsAndHashCode(of = {"iban"})
+@Table(indexes = {
+        @Index(name = "wallet_user_id_iban_key", columnList = "user_id, iban", unique = true),
+        @Index(name = "wallet_user_id_name_key", columnList = "user_id, name", unique = true)
+})
 public class Wallet {
 
     @Id
