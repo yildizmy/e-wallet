8de832bab9608291adbcaaaa2899af1081cf8fa9
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-24 09:45:39 +0300

    feat: implement jwt response

---
+++ backend/src/main/java/com/github/yildizmy/dto/response/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.github.yildizmy.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * Data Transfer Object used for authentication response
+ */
+@Getter
+@Builder
+public class JwtResponse {
+
+    private String type;
+    private String token;
+    private Long id;
+    private String username;
+    private List<String> roles;
+}
