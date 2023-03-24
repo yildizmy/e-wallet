7784fd839623752fbe9d541abee9771764da964b
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-24 09:48:45 +0300

    feat: implement type response

---
+++ backend/src/main/java/com/github/yildizmy/dto/response/TypeResponse.java
@@ -0,0 +1,14 @@
+package com.github.yildizmy.dto.response;
+
+import lombok.Data;
+
+/**
+ * Data Transfer Object for Type response
+ */
+@Data
+public class TypeResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+}
