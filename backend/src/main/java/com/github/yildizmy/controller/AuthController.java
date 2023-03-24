b3daff095b1b113af959f771a427a824b36c7dd4
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-24 09:45:01 +0300

    feat: implement auth controller

---
+++ backend/src/main/java/com/github/yildizmy/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.github.yildizmy.controller;
+
+import com.github.yildizmy.dto.request.LoginRequest;
+import com.github.yildizmy.dto.request.UserRequest;
+import com.github.yildizmy.dto.response.ApiResponse;
+import com.github.yildizmy.dto.response.CommandResponse;
+import com.github.yildizmy.dto.response.JwtResponse;
+import com.github.yildizmy.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Clock;
+import java.time.Instant;
+
+import static com.github.yildizmy.common.Constants.SUCCESS;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final Clock clock;
+    private final AuthService authService;
+
+    /**
+     * Authenticates users by their credentials
+     *
+     * @param request
+     * @return JwtResponse
+     */
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<JwtResponse>> login(@Valid @RequestBody LoginRequest request) {
+        final JwtResponse response = authService.login(request);
+        return ResponseEntity.ok(new ApiResponse<>(Instant.now(clock).toEpochMilli(), SUCCESS, response));
+    }
+
+    /**
+     * Registers users using their credentials and user info
+     *
+     * @param request
+     * @return id of the registered user
+     */
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<CommandResponse>> signup(@Valid @RequestBody UserRequest request) {
+        final CommandResponse response = authService.signup(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(Instant.now(clock).toEpochMilli(), SUCCESS, response));
+    }
+}
