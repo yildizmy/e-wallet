cf88397ee346a12988efaf10a7a85aaf7667db54
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-24 20:59:07 +0300

    feat: implement wallet repository

--- backend/src/main/java/com/github/yildizmy/repository/WalletRepository.java
+++ backend/src/main/java/com/github/yildizmy/repository/WalletRepository.java
@@ -4,6 +4,14 @@ import com.github.yildizmy.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
+    Optional<Wallet> findByIban(String iban);
+
+    boolean existsByUserIdAndIbanIgnoreCase(Long userId, String iban);
+
+    boolean existsByUserIdAndNameIgnoreCase(Long userId, String name);
 }
