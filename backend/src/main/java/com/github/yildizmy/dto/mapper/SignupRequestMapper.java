63aa1ce40f1c6c855498407ebc1736dbba4c9b7e
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 11:31:34 +0300

    feat: implement mapper for signup request

---
+++ backend/src/main/java/com/github/yildizmy/dto/mapper/SignupRequestMapper.java
@@ -0,0 +1,54 @@
+package com.github.yildizmy.dto.mapper;
+
+import com.github.yildizmy.dto.request.SignupRequest;
+import com.github.yildizmy.model.Role;
+import com.github.yildizmy.model.RoleType;
+import com.github.yildizmy.model.User;
+import com.github.yildizmy.service.RoleService;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Mapper used for mapping SignupRequest fields
+ */
+@Mapper(componentModel = "spring",
+        uses = {PasswordEncoder.class, RoleService.class},
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public abstract class SignupRequestMapper {
+
+    private PasswordEncoder passwordEncoder;
+    private RoleService roleService;
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    public void setRoleService(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Mapping(target = "firstName", expression = "java(org.apache.commons.text.WordUtils.capitalizeFully(dto.getFirstName()))")
+    @Mapping(target = "lastName", expression = "java(org.apache.commons.text.WordUtils.capitalizeFully(dto.getLastName()))")
+    @Mapping(target = "username", expression = "java(dto.getUsername().trim().toLowerCase())")
+    @Mapping(target = "email", expression = "java(dto.getEmail().trim().toLowerCase())")
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "roles", ignore = true)
+    public abstract User toEntity(SignupRequest dto);
+
+    @AfterMapping
+    void setToEntityFields(@MappingTarget User entity, SignupRequest dto) {
+        entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+        final List<RoleType> rTypes = dto.getRoles().stream()
+                .map(r -> RoleType.valueOf(r))
+                .toList();
+        final List<Role> roles = roleService.getReferenceByTypeIsIn(new HashSet<>(rTypes));
+        entity.setRoles(new HashSet<>(roles));
+    }
+}
