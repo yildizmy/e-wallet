3a1f62bcd13412118e5bec5f2bf079205a218c79
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 11:18:53 +0300

    feat: implement new method

--- backend/src/main/java/com/github/yildizmy/service/RoleService.java
+++ backend/src/main/java/com/github/yildizmy/service/RoleService.java
@@ -1,12 +1,14 @@
 package com.github.yildizmy.service;
 
 import com.github.yildizmy.model.Role;
+import com.github.yildizmy.model.RoleType;
 import com.github.yildizmy.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Service used for Role related operations
@@ -17,6 +19,16 @@ import java.util.List;
 public class RoleService {
 
     private final RoleRepository roleRepository;
+
+    /**
+     * Fetches list of role (entity) by the given role types
+     *
+     * @param types
+     * @return List of Role
+     */
+    public List<Role> getReferenceByTypeIsIn(Set<RoleType> types) {
+        return roleRepository.getReferenceByTypeIsIn(types);
+    }
 
     /**
      * Fetches all roles as entity
