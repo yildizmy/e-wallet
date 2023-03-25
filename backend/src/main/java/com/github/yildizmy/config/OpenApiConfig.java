fc3c87df8d84111d564eb467014901f9a49ea729
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 20:51:57 +0300

    feat: implement open-api documentation

---
+++ backend/src/main/java/com/github/yildizmy/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.github.yildizmy.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "e-wallet REST API", version = "1.0",
+        description = "Application used for a digital wallet",
+        contact = @Contact(name = "Murat Yıldız")),
+        security = {@SecurityRequirement(name = "bearerToken")}
+)
+@SecuritySchemes({
+        @SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT"),
+})
+public class OpenApiConfig {
+}
