af0ba109ad5969f922421d439729eb9daa575eda
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 12:33:54 +0300

    refactor: update log messages

--- backend/src/main/java/com/github/yildizmy/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/github/yildizmy/security/UserDetailsServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
+
 /**
  * Service used for UserDetails related operations
  */
@@ -23,7 +25,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         final User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format(Constants.NOT_FOUND_USERNAME, username)));
+                .orElseThrow(() -> new UsernameNotFoundException(MessageFormat.format(Constants.NOT_FOUND_USERNAME, username)));
         return UserDetailsImpl.build(user);
     }
 }
