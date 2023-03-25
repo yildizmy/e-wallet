fdd6935dc40a339cbcdc76a5287d2560d8e30db0
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 11:25:22 +0300

    feat: implement signup request

---
+++ backend/src/main/java/com/github/yildizmy/dto/request/SignupRequest.java
@@ -0,0 +1,40 @@
+package com.github.yildizmy.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+/**
+ * Data Transfer Object for Signup request
+ */
+@Data
+public class SignupRequest {
+
+    private Long id;
+
+    @Size(min = 3, max = 50, message = "{firstName.size}")
+    @NotBlank(message = "{firstName.notblank}")
+    private String firstName;
+
+    @Size(min = 3, max = 50, message = "{lastName.size}")
+    @NotBlank(message = "{lastName.notblank}")
+    private String lastName;
+
+    @Size(min = 3, max = 20, message = "{username.size}")
+    @NotBlank(message = "{username.notblank}")
+    private String username;
+
+    @Email(message = "email.acceptable")
+    @Size(min = 6, max = 50, message = "{email.size}")
+    @NotBlank(message = "email.notblank}")
+    private String email;
+
+    @Size(min = 6, max = 100, message = "{password.size}")
+    @NotBlank(message = "password.notblank}")
+    private String password;
+
+    private Set<String> roles;
+}
