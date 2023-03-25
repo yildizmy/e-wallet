e2f71c253eb741b0f69885d3ac32434e3ba3b5ba
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 11:25:45 +0300

    feat: implement new method

--- backend/src/main/java/com/github/yildizmy/repository/UserRepository.java
+++ backend/src/main/java/com/github/yildizmy/repository/UserRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    boolean existsByUsernameIgnoreCase(String name);
 }
