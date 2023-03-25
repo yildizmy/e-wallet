93a6ac27e76bab268b0caceca8d9ab721d920514
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 20:36:13 +0300

    refactor: format code

--- backend/src/main/java/com/github/yildizmy/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/github/yildizmy/security/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.github.yildizmy.security;
 
-import com.github.yildizmy.common.Constants;
 import com.github.yildizmy.model.User;
 import com.github.yildizmy.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -11,6 +10,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
+
+import static com.github.yildizmy.common.Constants.NOT_FOUND_USERNAME;
 
 /**
  * Service used for UserDetails related operations
@@ -25,7 +26,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         final User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(MessageFormat.format(Constants.NOT_FOUND_USERNAME, username)));
+                .orElseThrow(() -> new UsernameNotFoundException(MessageFormat.format(NOT_FOUND_USERNAME, username)));
         return UserDetailsImpl.build(user);
     }
 }
