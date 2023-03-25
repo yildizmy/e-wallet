58e6cce3c0ef8ce25a882c47d5e21b844c09396d
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-25 11:16:21 +0300

    feat: implement new method

--- backend/src/main/java/com/github/yildizmy/repository/RoleRepository.java
+++ backend/src/main/java/com/github/yildizmy/repository/RoleRepository.java
@@ -1,9 +1,15 @@
 package com.github.yildizmy.repository;
 
 import com.github.yildizmy.model.Role;
+import com.github.yildizmy.model.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    List<Role> getReferenceByTypeIsIn(Set<RoleType> types);
 }
