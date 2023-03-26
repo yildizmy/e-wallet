227f3ee1f328e7257543c411c3068011963663cf
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-26 17:53:25 +0300

    refactor: map full name

--- backend/src/main/java/com/github/yildizmy/dto/mapper/WalletResponseMapper.java
+++ backend/src/main/java/com/github/yildizmy/dto/mapper/WalletResponseMapper.java
@@ -1,8 +1,14 @@
 package com.github.yildizmy.dto.mapper;
 
+import com.github.yildizmy.dto.response.UserResponse;
 import com.github.yildizmy.dto.response.WalletResponse;
+import com.github.yildizmy.model.User;
 import com.github.yildizmy.model.Wallet;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.text.MessageFormat;
 
 /**
  * Mapper used for mapping WalletResponse fields
@@ -13,4 +19,9 @@ public interface WalletResponseMapper {
     Wallet toEntity(WalletResponse dto);
 
     WalletResponse toDto(Wallet entity);
+
+    @AfterMapping
+    default void setFullName(@MappingTarget UserResponse dto, User entity) {
+        dto.setFullName(MessageFormat.format("{0} {1}", entity.getFirstName(), entity.getLastName()));
+    }
 }
