53619d05fc55405dba9d3f93d60b6ef2e62e1c75
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-26 11:29:47 +0300

    feat: add frontend template

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+import { StyledChart } from './components/chart';
+import ScrollToTop from './components/scroll-to-top';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <ScrollToTop />
+          <StyledChart />
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
+}
