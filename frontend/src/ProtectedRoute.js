1ce92f05c802e65dd85ae32f0cbeebc7073434df
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-29 21:22:32 +0300

    refactor: update object destructuring

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,7 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import AuthService from './services/AuthService';
 
-const ProtectedRoute = ({ roles = [] }) => {
+const ProtectedRoute = (props) => {
+  const [roles] = props;
   const userRoles = AuthService.getCurrentUser()?.roles;
   const isAllowed = !roles.length || roles.some((r) => userRoles.includes(r));
 
