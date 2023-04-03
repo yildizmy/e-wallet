43c35418f1c02c7f6ebd664e04684f84f05c5278
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-03 12:04:26 +0300

    refactor: rename variable

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -4,9 +4,9 @@ import AuthService from './services/AuthService';
 const ProtectedRoute = (props) => {
   const [roles] = props;
   const userRoles = AuthService.getCurrentUser()?.roles;
-  const isAllowed = !roles.length || roles.some((r) => userRoles.includes(r));
+  const isAuthorized = !roles.length || roles.some((r) => userRoles.includes(r));
 
-  return isAllowed ? <Outlet /> : <Navigate to="/unauthorized" replace />;
+  return isAuthorized ? <Outlet /> : <Navigate to="/unauthorized" replace />;
 };
 
 export default ProtectedRoute;
