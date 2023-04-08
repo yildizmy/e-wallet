67559068a77fc40ddde486b2b08dd92c9cc1738e
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-09 02:51:49 +0300

    refactor: remove unused import

--- backend/src/main/java/com/github/yildizmy/config/OpenApiConfig.java
+++ backend/src/main/java/com/github/yildizmy/config/OpenApiConfig.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
