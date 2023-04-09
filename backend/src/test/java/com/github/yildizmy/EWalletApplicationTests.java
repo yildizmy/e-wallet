e7f74626d0c09bc8ebae6416e3a5ff3e2585e451
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-09 03:00:57 +0300

    refactor: suppress warning for not used test class

--- backend/src/test/java/com/github/yildizmy/EWalletApplicationTests.java
+++ backend/src/test/java/com/github/yildizmy/EWalletApplicationTests.java
@@ -3,6 +3,7 @@ package com.github.yildizmy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SuppressWarnings("squid:S2699")
 @SpringBootTest
 class EWalletApplicationTests {
 
