783d2102303f9c349791951ad922f3681b91b496
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:34:43 +0300

    refactor: format code

--- frontend/src/components/scroll-to-top/ScrollToTop.js
+++ frontend/src/components/scroll-to-top/ScrollToTop.js
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-// ----------------------------------------------------------------------
-
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
