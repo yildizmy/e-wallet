1d8df1117e921a72751268593f91499e31cc1788
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:36:56 +0300

    refactor: format code

--- frontend/src/hooks/useResponsive.js
+++ frontend/src/hooks/useResponsive.js
@@ -1,8 +1,5 @@
-// @mui
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-
-// ----------------------------------------------------------------------
 
 export default function useResponsive(query, start, end) {
   const theme = useTheme();
