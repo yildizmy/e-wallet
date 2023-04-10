af8c4af4aaa3a91b6ac061869246f5b011b82b6b
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:30:27 +0300

    refactor: format code

--- frontend/src/layouts/dashboard/nav/config.js
+++ frontend/src/layouts/dashboard/nav/config.js
@@ -1,7 +1,4 @@
-// component
 import SvgColor from '../../../components/svg-color';
-
-// ----------------------------------------------------------------------
 
 const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
 
