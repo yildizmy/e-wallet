545956d719d3219cca9839656b4ab80b82c140f1
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:32:04 +0300

    refactor: format code

--- frontend/src/components/iconify/Iconify.js
+++ frontend/src/components/iconify/Iconify.js
@@ -1,11 +1,7 @@
 import PropTypes from 'prop-types';
 import { forwardRef } from 'react';
-// icons
 import { Icon } from '@iconify/react';
-// @mui
 import { Box } from '@mui/material';
-
-// ----------------------------------------------------------------------
 
 const Iconify = forwardRef(({ icon, width = 20, sx, ...other }, ref) => (
   <Box ref={ref} component={Icon} icon={icon} sx={{ width, height: width, ...sx }} {...other} />
