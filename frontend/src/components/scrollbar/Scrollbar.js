2386e80f34329514d0338f8c7c332d9e2a83f766
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:34:34 +0300

    refactor: format code

--- frontend/src/components/scrollbar/Scrollbar.js
+++ frontend/src/components/scrollbar/Scrollbar.js
@@ -1,11 +1,7 @@
 import PropTypes from 'prop-types';
 import { memo } from 'react';
-// @mui
 import { Box } from '@mui/material';
-//
 import { StyledRootScrollbar, StyledScrollbar } from './styles';
-
-// ----------------------------------------------------------------------
 
 Scrollbar.propTypes = {
   sx: PropTypes.object,
