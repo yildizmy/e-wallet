95a9aa5900353725cdc7949db622c2829ea05199
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:32:37 +0300

    refactor: remove unused part and format code

--- frontend/src/layouts/dashboard/nav/index.js
+++ frontend/src/layouts/dashboard/nav/index.js
@@ -1,25 +1,14 @@
+import { Box, Drawer } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-// @mui
-import { styled, alpha } from '@mui/material/styles';
-import { Box, Link, Button, Drawer, Typography, Avatar, Stack } from '@mui/material';
-// mock
-import account from '../../../_mock/account';
-// hooks
-import useResponsive from '../../../hooks/useResponsive';
-// components
 import Logo from '../../../components/logo';
-import Scrollbar from '../../../components/scrollbar';
 import NavSection from '../../../components/nav-section';
-//
+import Scrollbar from '../../../components/scrollbar';
+import useResponsive from '../../../hooks/useResponsive';
 import navConfig from './config';
 
-// ----------------------------------------------------------------------
-
 const NAV_WIDTH = 280;
-
-// ----------------------------------------------------------------------
 
 Nav.propTypes = {
   openNav: PropTypes.bool,
@@ -48,29 +37,8 @@ export default function Nav({ openNav, onCloseNav }) {
       <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
         <Logo />
       </Box>
-
-      {/* <Box sx={{ mb: 5, mx: 2.5 }}>
-        <Link underline="none">
-          <StyledAccount>
-            <Avatar src={account.photoURL} alt="photoURL" />
-
-            <Box sx={{ ml: 2 }}>
-              <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-                {account.displayName}
-              </Typography>
-
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                {account.role}
-              </Typography>
-            </Box>
-          </StyledAccount>
-        </Link>
-      </Box> */}
-
       <NavSection data={navConfig} />
-
       <Box sx={{ flexGrow: 1 }} />
-
     </Scrollbar>
   );
 
