1d3443edd3634ad10d0777f929fbb7cff476d393
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:32:56 +0300

    refactor: format code

--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -1,17 +1,13 @@
 import PropTypes from 'prop-types';
 import { useMemo } from 'react';
-// @mui
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
-//
 import palette from './palette';
 import shadows from './shadows';
 import typography from './typography';
 import GlobalStyles from './globalStyles';
 import customShadows from './customShadows';
 import componentsOverride from './overrides';
-
-// ----------------------------------------------------------------------
 
 ThemeProvider.propTypes = {
   children: PropTypes.node,
