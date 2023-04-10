0e66d95de77857c3c88e440a81126e0960a20a91
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:32:15 +0300

    refactor: format code

--- frontend/src/layouts/dashboard/header/index.js
+++ frontend/src/layouts/dashboard/header/index.js
@@ -1,16 +1,10 @@
 import PropTypes from 'prop-types';
-// @mui
 import { styled } from '@mui/material/styles';
 import { Box, Stack, AppBar, Toolbar, IconButton } from '@mui/material';
-// utils
 import { bgBlur } from '../../../utils/cssStyles';
-// components
 import Iconify from '../../../components/iconify';
-//
 import Searchbar from './Searchbar';
 import AccountPopover from './AccountPopover';
-
-// ----------------------------------------------------------------------
 
 const NAV_WIDTH = 280;
 
