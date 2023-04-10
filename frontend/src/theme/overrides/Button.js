02d416f3157712222a0d299f791ff6a635e382f2
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-11 01:30:08 +0300

    refactor: format code

--- frontend/src/theme/overrides/Button.js
+++ frontend/src/theme/overrides/Button.js
@@ -1,7 +1,5 @@
 import { alpha } from '@mui/material/styles';
 
-// ----------------------------------------------------------------------
-
 export default function Button(theme) {
   return {
     MuiButton: {
