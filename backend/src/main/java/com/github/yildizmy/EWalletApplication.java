c1bf9d46b4503f43785c48c6b9f1f4aa26b7180e
Murat Yıldız <murat.yildiz@kuehne-nagel.com>
2024-05-03 00:42:51 +0300

    add logging for ApplicationStartup

--- backend/src/main/java/com/github/yildizmy/EWalletApplication.java
+++ backend/src/main/java/com/github/yildizmy/EWalletApplication.java
@@ -1,13 +1,38 @@
 package com.github.yildizmy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+@Slf4j(topic = "EWalletApplication")
 @SpringBootApplication
 public class EWalletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EWalletApplication.class, args);
+		SpringApplication app = new SpringApplication(EWalletApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logApplicationStartup(env);
 	}
 
+	private static void logApplicationStartup(Environment env) {
+		String protocol = (env.getProperty("server.ssl.key-store") != null) ? "https" : "http";
+		String serverPort = env.getProperty("server.port");
+		String contextPath = env.getProperty("server.servlet.context-path", "/");
+		String hostAddress = "localhost";
+		try {
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			log.warn("The host name could not be determined, using `localhost` as fallback");
+		}
+		log.info("\n" +
+				"-------------------------------------------------------\n" +
+				"Application '" + env.getProperty("spring.application.name") + "' is running! Access URLs:\n" +
+				"Local:      " + protocol + "://localhost:" + serverPort + contextPath + "\n" +
+				"External:   " + protocol + "://" + hostAddress + ":" + serverPort + contextPath + "\n" +
+				"Profile(s): " + String.join(",", env.getActiveProfiles()) + "\n" +
+				"-------------------------------------------------------");
+	}
 }
