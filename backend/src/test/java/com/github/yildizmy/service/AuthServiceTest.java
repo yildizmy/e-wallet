92f4f0a69c7bf16d6f04ccfb743f3f0a49127e01
Murat Yildiz <murat.yildiz@enefit.ee>
2024-10-02 21:57:35 +0300

    Implement new unit test for AuthService

--- backend/src/test/java/com/github/yildizmy/service/AuthServiceTest.java
+++ backend/src/test/java/com/github/yildizmy/service/AuthServiceTest.java
@@ -1,7 +1,12 @@
 package com.github.yildizmy.service;
 
+import com.github.yildizmy.dto.mapper.SignupRequestMapper;
 import com.github.yildizmy.dto.request.LoginRequest;
+import com.github.yildizmy.dto.request.SignupRequest;
+import com.github.yildizmy.dto.response.CommandResponse;
 import com.github.yildizmy.dto.response.JwtResponse;
+import com.github.yildizmy.model.User;
+import com.github.yildizmy.repository.UserRepository;
 import com.github.yildizmy.security.JwtUtils;
 import com.github.yildizmy.security.UserDetailsImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +21,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collections;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -29,6 +35,12 @@ class AuthServiceTest {
 
     @Mock
     private JwtUtils jwtUtils;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private SignupRequestMapper signupRequestMapper;
 
     @InjectMocks
     private AuthService authService;
@@ -62,5 +74,28 @@ class AuthServiceTest {
 
         verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
         verify(jwtUtils).generateJwtToken(authentication);
+    }
+
+    @Test
+    void signup_shouldCreateNewUser() {
+        SignupRequest signupRequest = new SignupRequest(1L, "New", "User", "newuser", "new@example.com", "password", Set.of("ROLE_USER"));
+        User newUser = new User();
+        newUser.setId(2L);
+        newUser.setUsername("newuser");
+
+        when(userRepository.existsByUsernameIgnoreCase("newuser")).thenReturn(false);
+        when(userRepository.existsByEmailIgnoreCase("new@example.com")).thenReturn(false);
+        when(signupRequestMapper.toEntity(signupRequest)).thenReturn(newUser);
+        when(userRepository.save(newUser)).thenReturn(newUser);
+
+        CommandResponse response = authService.signup(signupRequest);
+
+        assertNotNull(response);
+        assertEquals(2L, response.id());
+
+        verify(userRepository).existsByUsernameIgnoreCase("newuser");
+        verify(userRepository).existsByEmailIgnoreCase("new@example.com");
+        verify(signupRequestMapper).toEntity(signupRequest);
+        verify(userRepository).save(newUser);
     }
 }
