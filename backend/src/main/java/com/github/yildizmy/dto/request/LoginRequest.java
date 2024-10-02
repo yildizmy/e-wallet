87fd10400360ddc43ca65da7fda6fac86047e6a1
Murat Yildiz <murat.yildiz@enefit.ee>
2024-10-02 21:53:41 +0300

    Add constructor to the request

--- backend/src/main/java/com/github/yildizmy/dto/request/LoginRequest.java
+++ backend/src/main/java/com/github/yildizmy/dto/request/LoginRequest.java
@@ -2,12 +2,16 @@ package com.github.yildizmy.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Data Transfer Object for Login request
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 
     @Size(min = 3, max = 20, message = "{username.size}")
