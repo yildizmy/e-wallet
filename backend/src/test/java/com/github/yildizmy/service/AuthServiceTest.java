66dfaf4365d6a1c3692064eeb46df34aef76bd2b
Murat Yildiz <murat.yildiz@bwt.ee>
2025-04-27 13:44:58 +0300

    refactor test using `var` instead of type

--- backend/src/test/java/com/github/yildizmy/service/AuthServiceTest.java
+++ backend/src/test/java/com/github/yildizmy/service/AuthServiceTest.java
@@ -5,8 +5,6 @@ import com.github.yildizmy.domain.entity.User;
 import com.github.yildizmy.dto.mapper.SignupRequestMapper;
 import com.github.yildizmy.dto.request.LoginRequest;
 import com.github.yildizmy.dto.request.SignupRequest;
-import com.github.yildizmy.dto.response.CommandResponse;
-import com.github.yildizmy.dto.response.JwtResponse;
 import com.github.yildizmy.exception.ElementAlreadyExistsException;
 import com.github.yildizmy.repository.UserRepository;
 import com.github.yildizmy.security.JwtUtils;
@@ -56,18 +54,29 @@ class AuthServiceTest {
     @BeforeEach
     void setUp() {
         loginRequest = new LoginRequest("testuser", "password");
-        userDetails = new UserDetailsImpl(1L, "testuser", "password", "Test", "User",
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
-        authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        userDetails = new UserDetailsImpl(
+                1L,
+                "testuser",
+                "password",
+                "Test",
+                "User",
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+        authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
     }
 
     @Test
     void login_shouldReturnJwtResponse() {
-        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(
-                authentication);
-        when(jwtUtils.generateJwtToken(authentication)).thenReturn("test.jwt.token");
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(authentication);
+        when(jwtUtils.generateJwtToken(authentication))
+                .thenReturn("test.jwt.token");
 
-        JwtResponse response = authService.login(loginRequest);
+        var response = authService.login(loginRequest);
 
         assertNotNull(response);
         assertEquals("test.jwt.token", response.getToken());
@@ -76,15 +85,23 @@ class AuthServiceTest {
         assertEquals("Test", response.getFirstName());
         assertEquals("User", response.getLastName());
         assertEquals(Collections.singletonList("ROLE_USER"), response.getRoles());
+
         verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
         verify(jwtUtils).generateJwtToken(authentication);
     }
 
     @Test
     void signup_shouldCreateNewUser() {
-        SignupRequest signupRequest = new SignupRequest(1L, "New", "User", "newuser",
-                "new@example.com", "password", Set.of("ROLE_USER"));
-        User newUser = new User();
+        var signupRequest = new SignupRequest(
+                1L,
+                "New",
+                "User",
+                "newuser",
+                "new@example.com",
+                "password",
+                Set.of("ROLE_USER")
+        );
+        var newUser = new User();
         newUser.setId(2L);
         newUser.setUsername("newuser");
 
@@ -93,10 +110,11 @@ class AuthServiceTest {
         when(signupRequestMapper.toUser(signupRequest)).thenReturn(newUser);
         when(userRepository.save(newUser)).thenReturn(newUser);
 
-        CommandResponse response = authService.signup(signupRequest);
+        var response = authService.signup(signupRequest);
 
         assertNotNull(response);
         assertEquals(2L, response.id());
+
         verify(userRepository).existsByUsernameIgnoreCase("newuser");
         verify(userRepository).existsByEmailIgnoreCase("new@example.com");
         verify(signupRequestMapper).toUser(signupRequest);
@@ -105,9 +123,16 @@ class AuthServiceTest {
 
     @Test
     void signup_shouldThrowExceptionWhenUsernameExists() {
-        SignupRequest signupRequest =
-                new SignupRequest(1L, "Existing", "User", "existinguser",
-                        "existing@example.com", "password", Set.of("ROLE_USER"));
+        var signupRequest = new SignupRequest(
+                1L,
+                "Existing",
+                "User",
+                "existinguser",
+                "existing@example.com",
+                "password",
+                Set.of("ROLE_USER")
+        );
+
         when(userRepository.existsByUsernameIgnoreCase("existinguser")).thenReturn(true);
 
         assertThrows(ElementAlreadyExistsException.class, () -> authService.signup(signupRequest));
@@ -120,9 +145,16 @@ class AuthServiceTest {
 
     @Test
     void signup_shouldThrowExceptionWhenEmailExists() {
-        SignupRequest signupRequest =
-                new SignupRequest(1L, "New", "User", "newuser",
-                        "existing@example.com", "password", Set.of("ROLE_USER"));
+        var signupRequest = new SignupRequest(
+                1L,
+                "New",
+                "User",
+                "newuser",
+                "existing@example.com",
+                "password",
+                Set.of("ROLE_USER")
+        );
+
         when(userRepository.existsByUsernameIgnoreCase("newuser")).thenReturn(false);
         when(userRepository.existsByEmailIgnoreCase("existing@example.com")).thenReturn(true);
 
